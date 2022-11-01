e80129bc9c6c0b52830f3f62a6cb4ba4bef2717a
Rodrigo Almeida <rodrigo.ddeus.almeida@gmail.com>
2022-11-01 09:28:05 -0300

    Adding api integration in company page

--- src/views/tables/TableCompany.tsx
+++ src/views/tables/TableCompany.tsx
@@ -1,5 +1,5 @@
 // ** React Imports
-import { useState, ChangeEvent } from 'react'
+import { useState, ChangeEvent, useEffect } from 'react'
 
 // ** MUI Imports
 import Paper from '@mui/material/Paper'
@@ -14,80 +14,42 @@ import TablePagination from '@mui/material/TablePagination'
 import useTranslation from 'next-translate/useTranslation'
 
 import GetAll from 'src/api/company/CompanyService'
+const URL_API = "https://gbpjn8sulk.execute-api.us-east-1.amazonaws.com/prod/company"
 
 interface Column {
-  id: 'name' | 'abbreviation' | 'population' | 'size' | 'density'
+  id: 'name' | 'abbreviation' | 'email' 
   label: string
   minWidth?: number
   align?: 'right'
   format?: (value: number) => string
 }
-const x = GetAll();
+
 
 const columns: readonly Column[] = [
   
   { id: 'name', label: 'name', minWidth: 170 },
   { id: 'abbreviation', label: 'company_abbreviation', minWidth: 100 },
-  {
-    id: 'population',
-    label: 'Population',
-    minWidth: 170,
-    align: 'right',
-    format: (value: number) => value.toLocaleString('en-US')
-  },
-  {
-    id: 'size',
-    label: 'Size\u00a0(km\u00b2)',
-    minWidth: 170,
-    align: 'right',
-    format: (value: number) => value.toLocaleString('en-US')
-  },
-  {
-    id: 'density',
-    label: 'Density',
-    minWidth: 170,
-    align: 'right',
-    format: (value: number) => value.toFixed(2)
-  }
+  { id: 'email', label: 'Email'}
 ]
 
 interface Data {
   name: string
   abbreviation: string
-  size: number
-  density: number
-  population: number
+  email: string
 }
 
-function createData(name: string, abbreviation: string, population: number, size: number): Data {
-  const density = population / size
-
-  return { name, abbreviation, population, size, density }
+function createData(name: string, abbreviation: string, email: string, size: number): Data {
+  return { name, abbreviation, email }
 }
 
-const rows = [
-  createData('India', 'IN', 1324171354, 3287263),
-  createData('China', 'CN', 1403500365, 9596961),
-  createData('Italy', 'IT', 60483973, 301340),
-  createData('United States', 'US', 327167434, 9833520),
-  createData('Canada', 'CA', 37602103, 9984670),
-  createData('Australia', 'AU', 25475400, 7692024),
-  createData('Germany', 'DE', 83019200, 357578),
-  createData('Ireland', 'IE', 4857000, 70273),
-  createData('Mexico', 'MX', 126577691, 1972550),
-  createData('Japan', 'JP', 126317000, 377973),
-  createData('France', 'FR', 67022000, 640679),
-  createData('United Kingdom', 'GB', 67545757, 242495),
-  createData('Russia', 'RU', 146793744, 17098246),
-  createData('Nigeria', 'NG', 200962417, 923768),
-  createData('Brazil', 'BR', 210147125, 8515767)
-]
 
 const TableStickyHeader = () => {
   // ** States
   const { t } = useTranslation('common');
   const [page, setPage] = useState<number>(0)
   const [rowsPerPage, setRowsPerPage] = useState<number>(10)
+  const [data, setData] = useState<Data[]>([])
+  const [loading, setLoading] = useState(false)
 
   const handleChangePage = (event: unknown, newPage: number) => {
     setPage(newPage)
@@ -98,6 +60,24 @@ const TableStickyHeader = () => {
     setPage(0)
   }
 
+  const GetAll = async () => {
+    try{
+      setLoading(true)
+      const response = await fetch(URL_API)
+      const data = await response.json()
+      setData(data)
+    }catch(error){
+      console.log(error)
+    }finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    GetAll()
+  }, [])
+
+  
   return (
     <Paper sx={{ width: '100%', overflow: 'hidden' }}>
       <TableContainer sx={{ maxHeight: 440 }}>
@@ -112,7 +92,7 @@ const TableStickyHeader = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => {
+            {data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => {
               return (
                 <TableRow hover role='checkbox' tabIndex={-1} key={row.abbreviation}>
                   {columns.map(column => {
@@ -130,15 +110,6 @@ const TableStickyHeader = () => {
           </TableBody>
         </Table>
       </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component='div'
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
     </Paper>
   )
 }
